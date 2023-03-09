729cf2bb36c00b0dff1b54aea0fbcc4bbadb7c09
MichaelBalcerzak <balcerzak.michael@gmail.com>
2023-03-09 16:49:17 -0500

    working on 2

--- assignment3.py
+++ assignment3.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
 import sklearn
+from sklearn.model_selection import train_test_split
 
 
 # Keras data generator for the hill vallye dataset
@@ -12,20 +13,18 @@ class HillValleyDataGenerator(tf.keras.utils.Sequence):
     def __init__(self, dataset_filepath, batch_size):
         # dataset_filepath is the path to a .data file containing the dataset
         # batch_size is the batch size for the network
-        model = keras.Sequential()
-        model.add(keras.layers.Dense(3, imput_shape=(4,), activation="relu"))
 
-        WS = pd.read_excel(dataset_filepath)
+        WS = pd.read_csv(dataset_filepath)
         data = np.array(WS)
         x = data[:, :-1]
         self.x = x / x.max(axis=0)
         self.y = data[:, -1]
 
-        self.x_train, self.x_test, self.y_train, self.y_test = sklearn.model_selection.train_test_split(self.x, self.y, test_size=0.2)
+        self.x_train, self.x_test, self.y_train, self.y_test = sklearn.model_selection.train_test_split(self.x, self.y,
+                                                                                                        test_size=0.2)
 
         self.batch_size = batch_size
         self.epochs = 5
-        self.model = model
 
         # Return nothing
 
@@ -51,16 +50,30 @@ class HillValleyDataGenerator(tf.keras.utils.Sequence):
 def hill_valley_cnn_model(dataset_filepath):
     # dataset_filepath is the path to a .data file containing the dataset
 
-    model = HillValleyDataGenerator(dataset_filepath, 20)
+    hillValleyModel = HillValleyDataGenerator(dataset_filepath, 20)
 
-    model.add(keras.layers.Conv2D(1, 5, input_shape=(100, 100, 1)))
+    model = keras.Sequential()
 
+    model.add(keras.layers.Conv2D(5, 5, input_shape=(10, 10, 1)))
+    model.add(keras.layers.MaxPooling2D(pool_size=2))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(50, activation='softmax'))
 
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    training_performance = 0
+
+    validation_performance = 0
 
     # model is a trained keras rnn model for predicting compressive strength
     # training_performance is the performance of the model on the training set
     # validation_performance is the performance of the model on the validation set
     return model, training_performance, validation_performance
+
+
+hill_valley_cnn_model("Hill_Valley_with_noise_Training.data")
 
 
 # A function that creates a keras rnn model to predict whether a sequence has a hill or valley

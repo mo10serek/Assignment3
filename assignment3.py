7363a9977ba0cb18cde6f675468e0129d4f8091c
MichaelBalcerzak <balcerzak.michael@gmail.com>
2023-03-11 10:16:27 -0500

    fix on the class, so it is related to the prof example and fix the function

--- assignment3.py
+++ assignment3.py
@@ -5,24 +5,23 @@ from tensorflow import keras
 import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
+import os
+
+cwd = os.getcwd()
 
 
 # Keras data generator for the hill vallye dataset
 class HillValleyDataGenerator(tf.keras.utils.Sequence):
-
     def __init__(self, dataset_filepath, batch_size):
         # dataset_filepath is the path to a .data file containing the dataset
         # batch_size is the batch size for the network
 
-        WS = pd.read_csv(dataset_filepath)
-        data = np.array(WS)
-        x = data[:, :-1]
-        self.x = x / x.max(axis=0)
-        self.y = data[:, -1]
+        self.data = pd.read_csv(dataset_filepath, sep=",")
 
-        self.x_train, self.x_test, self.y_train, self.y_test = sklearn.model_selection.train_test_split(self.x, self.y,
-                                                                                                        test_size=0.2)
+        self.x = self.data.values[:, 0:-1]
+        self.y = self.data['class'].values
 
+        self.number_of_images = len(self.x)
         self.batch_size = batch_size
         self.epochs = 5
 
@@ -31,7 +30,7 @@ class HillValleyDataGenerator(tf.keras.utils.Sequence):
     def __len__(self):
         # batches per epoch is the total number of batches used for one epoch
 
-        batches_per_epoch = self.batch_size // self.epochs
+        batches_per_epoch = self.number_of_images / self.batch_size
 
         return batches_per_epoch
 
@@ -50,22 +49,29 @@ class HillValleyDataGenerator(tf.keras.utils.Sequence):
 def hill_valley_cnn_model(dataset_filepath):
     # dataset_filepath is the path to a .data file containing the dataset
 
-    hillValleyModel = HillValleyDataGenerator(dataset_filepath, 20)
+    trainingHillValleyGenerator = HillValleyDataGenerator(dataset_filepath + '/Hill_Valley_with_noise_Training.data',
+                                                          20)
+    testHillValleyGenerator = HillValleyDataGenerator(dataset_filepath + '/Hill_Valley_with_noise_Testing.data', 20)
 
     model = keras.Sequential()
 
-    model.add(keras.layers.Conv2D(5, 5, input_shape=(10, 10, 1)))
+    model.add(keras.layers.InputLayer(input_shape=(10, 10, 3)))
+    model.add(keras.layers.Conv2D(filters=16, kernel_size=5, activation='sigmoid'))
+    model.add(keras.layers.MaxPooling2D(pool_size=2))
     model.add(keras.layers.MaxPooling2D(pool_size=2))
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(50, activation='softmax'))
+    model.add(keras.layers.Dense(256, activation="relu"))
+    model.add(keras.layers.Dense(102, activation='softmax'))
 
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
 
-    training_performance = 0
+    training_performance = 0  # model.fit(trainingHillValleyGenerator,
+    # epochs = 5,
+    # verbose = 2)
 
-    validation_performance = 0
+    validation_performance = 0  # model.evaluate(testHillValleyGenerator)
 
     # model is a trained keras rnn model for predicting compressive strength
     # training_performance is the performance of the model on the training set
@@ -73,7 +79,7 @@ def hill_valley_cnn_model(dataset_filepath):
     return model, training_performance, validation_performance
 
 
-hill_valley_cnn_model("Hill_Valley_with_noise_Training.data")
+hill_valley_cnn_model(cwd)
 
 
 # A function that creates a keras rnn model to predict whether a sequence has a hill or valley
